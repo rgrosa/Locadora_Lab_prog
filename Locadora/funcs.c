93cd6c9b2c1a8b5141668467593eeda021881092
VlNlClUS <56849654+VlNlClUS@users.noreply.github.com>
2019-11-14 15:50:09 -0300

    atualizado edit e busca

--- Locadora/funcs.c
+++ Locadora/funcs.c
@@ -1,5 +1,4 @@
 #include <time.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,35 +6,158 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include "funcs.h"
+    #include <string.h>
 
-void loadClientes(struct Cliente *c)
+void editF(int idf){
+
+   
+  
+if (fl[idf].fil_id==0){
+
+
+    printf("\n\nID INEXISTENTE\n\n");
+}
+else{
+
+
+
+    printf("############EDITAR############\n");
+
+    printf("\n\nNome do FILME %s editar para -> ", fl[idf].fil_nome);
+        fflush(stdin);
+		getchar();
+	    fgets(fl[idf].fil_nome, 40, stdin);
+
+    printf("SINOPSE: %s editar para -> ", fl[idf].fil_sinopses);
+        fflush(stdin);
+		getchar();
+		fgets(fl[idf].fil_sinopses, 1000, stdin);
+
+   
+
+    printf("Quantidade Disponivel em estoque: %d para -> \n\n", fl[idf].fil_qtd_disponivel);
+        scanf("%d",&fl[idf].fil_qtd_disponivel);
+                
+                 writeFilmes();
+}}
+void printF(int idf){
+ 
+  idf--;
+printf("\n\nNome do FILME %s", fl[idf].fil_nome);
+printf("ID: %d\n", fl[idf].fil_id);
+printf("SINOPSE: %s", fl[idf].fil_sinopses);
+printf("Quantidade Disponivel em estoque: %d\n\n", fl[idf].fil_qtd_disponivel);
+
+
+}
+
+
+
+
+
+int buscaF(char busc[]){
+
+
+
+int i, item;
+int j=0;
+
+   char *psResultado;
+
+ for(i=0; i<100; i++){
+   psResultado = strstr(fl[i].fil_nome, busc);
+
+        if(psResultado!= NULL){
+
+            printf("NOME: %s", fl[i].fil_nome);
+            printf("ID: %d\n\n", fl[i].fil_id);
+
+j++;
+ }     else {}}
+   if(j==0){printf("\n\nNENHUM ITEM ENCONTRADO\n\n");
+   
+                         return 0;
+   
+   }else{printf("\n\nFORAM ENCONTRADOS %d ITEN(S) CORRESPONDENTE(S)\n\n", j);
+   
+   
+   printf("\nDIGITE O ID CORRESPONDENTE A O ITEM PROCURADO\n");
+   scanf("%d", &item);
+   
+   
+   }
+   printF(item);
+                         return 0;
+}
+
+
+
+
+
+void loadClientes()
 {
 
     int red;
-    red = open("Banco/Clientes.txt", O_RDWR|O_CREAT, 0666);
+    red = open("BancoC.txt", O_RDWR|O_CREAT, 0666);
 
-    printf("%d\n",red);
+   
 
         if(red < 0){
             printf("ERROR\n");
         }
 
-    read(red, c, sizeof(struct Cliente) * 1000);
+    read(red, &cl, sizeof(struct cliente) * 1000);
     close(red);
 }
 
-void loadFilmes(struct Filme *f)
+
+void loadFilmes()
 {
 
     int red;
-    red = open("Banco/Filmes.txt", O_RDWR|O_CREAT, 0666);
+    red = open("BancoF.txt", O_RDWR|O_CREAT, 0666);
 
-    printf("%d\n",red);
+   
 
         if(red < 0){
             printf("ERROR\n");
         }
 
-    read(red, f, sizeof(struct Filme) * 1000);
+    read(red, &fl, sizeof(struct filme) * 1000);
     close(red);
 }
+
+void writeClientes(void)
+{
+
+    int red;
+    red = open("BancoC.txt", O_RDWR|O_CREAT, 0666);
+
+   
+
+        if(red < 0){
+            printf("ERROR\n");
+        }
+
+    write(red,&cl, sizeof(struct cliente) * 1000);
+    close(red);
+
+}
+
+void writeFilmes(void)
+{
+
+    int red;
+    red = open("BancoF.txt", O_RDWR|O_CREAT, 0666);
+
+   
+
+        if(red < 0){
+            printf("ERROR\n");
+        }
+
+    write(red,&fl, sizeof(struct filme) * 1000);
+    close(red);
+
+}
+ 
--- Locadora/main.c
+++ Locadora/main.c
@@ -2,16 +2,18 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
-
 #include "funcs.h"
-
+#include <string.h>
+#include <locale.h>
 void main(){
-	int aux, verif = 0, sizeCli = 0 /*Colocar número de clientes atuais aqui*/, sizeFil = 0 /*Colocar número de filmes atuais aqui*/, sizeLoc = 0 /*Colocar número de locações atuais aqui*/;
-	struct Cliente ListaClientes[100];
-	struct Filme ListaFilmes[100];
-	struct Locacao ListaLocacao[100];
+	int verif = 0;
 	char val;
+	int i,j,id;
+	char txc[10];
+
+loadFilmes();
 	system("@cls||clear");
+	
 
 	printf("==================================================\n");
 	printf("BEM VINDOS A LOCADORA 9/10 PORQUE NEM TUDO É PERFEITO\n");
@@ -20,8 +22,9 @@ void main(){
 	printf(" *o*o* \n");
 	printf("*o***o* \n");
 	printf("   |\n");
+	printf("\n");
 	printf("Feliz Natal e um ótimo ano novo!! Aceitamos nota de presente ou um vale refeição.\n");
-
+		
 	while(verif != 99){
 		printf("==================================================\n");
 		printf("Escolha uma opção:\n");
@@ -44,47 +47,46 @@ void main(){
 					scanf("%d", &verif);
 					switch(verif){
 						case 1:
-							system("@cls||clear");
-							printf("Digite o nome do cliente:\n");
-							scanf("%c", &val);
-							gets(ListaClientes[sizeCli].cli_nome);
-							system("@cls||clear");
-							printf("Digite o CPF (Somente números):\n");
-							scanf("%ld", &ListaClientes[sizeCli].cli_cpf);
-							system("@cls||clear");
-							printf("Digite a idade:\n");
-							scanf("%d", &ListaClientes[sizeCli].cli_idade);
-							system("@cls||clear");
-							printf("Digite o endereço:\n");
-							scanf("%c", &val);
-							gets(ListaClientes[sizeCli].cli_endereco);
-							ListaClientes[sizeCli].cli_id = sizeCli;
-							sizeCli+=1;
-							system("@cls||clear");
-							printf("==================================================\n");
-							printf("Cliente cadastrado com sucesso.\n");
+					
+for(i=1; i<100;i++){
+
+if (fl[i].fil_id== 0) {
+
+
+					printf("Digite o nome do cliente: ");
+				   		fflush(stdin);
+				 		getchar();
+						fgets(cl[i].cli_nome, 40, stdin);
+						system("@cls||clear");
+					printf("Digite o endereço do cliente: ");
+				   		fflush(stdin);
+				 		getchar();
+						fgets(cl[i].cli_endereco, 40, stdin);
+					printf("Digite o ID do cliente: ");
+						scanf("%d",&cl[i].cli_id);
+					printf("Digite CPF do cliente (SOMENTE NUMEROS): ");
+						scanf("%d",&cl[i].cli_cpf);
+					printf("Digite a idade do cliente: ");
+						scanf("%d",&cl[i].cli_idade);		
+						writeClientes();
+i=101;
+					
+					}
+}
+						
+						printf("==================================================\n");
+						printf("Cliente cadastrado com sucesso.\n");
 						break;
 						case 2:
-							system("@cls||clear");
-							printf("Digite o código do cliente à editar:\n");
-							scanf("%d", &aux);
-							system("@cls||clear");
-							printf("Digite o novo nome do cliente | Atual: %s:\n", ListaClientes[aux].cli_nome);
-							scanf("%c", &val);
-							gets(ListaClientes[aux].cli_nome);
-							system("@cls||clear");
-							printf("Digite o novo CPF (Somente números) | Atual: %ld:\n", ListaClientes[aux].cli_cpf);
-							scanf("%ld", &ListaClientes[aux].cli_cpf);
-							system("@cls||clear");
-							printf("Digite a nova idade | Atual: %d:\n", ListaClientes[aux].cli_idade);
-							scanf("%d", &ListaClientes[aux].cli_idade);
-							system("@cls||clear");
-							printf("Digite o novo endereço | Atual: %s:\n", ListaClientes[aux].cli_endereco);
-							scanf("%c", &val);
-							gets(ListaClientes[aux].cli_endereco);
-							printf("==================================================\n");
-							printf("Cliente alterado com sucesso.\n");
+
+										
+
+
 						break;
+						
+
+	
+						
 						case 99:
 							system("@cls||clear");
 							printf("==================================================\n");
@@ -109,76 +111,110 @@ void main(){
 					printf("2 - Editar filme;\n");
 					printf("3 - Procurar filme;\n");
 					printf("4 - Consultar informações;\n");
+					
+						
 					printf("99 - Voltar;\n");
 					printf("==================================================\n");
 					scanf("%d", &verif);
 					switch(verif){
 						case 1:
-							system("@cls||clear");
-							printf("Digite o nome do filme:\n");
-							scanf("%c", &val);
-							gets(ListaFilmes[sizeFil].fil_nome);
-							system("@cls||clear");
-							printf("Digite a classificação:\n");
-							scanf("%d", &ListaFilmes[sizeFil].fil_classificacao);
-							system("@cls||clear");
-							printf("Digite o gênero:\n");
-							scanf("%c", &val);
-							gets(ListaFilmes[sizeFil].fil_genero);
-							system("@cls||clear");
-							printf("Digite a sinopses:\n");
-							scanf("%c", &val);
-							gets(ListaFilmes[sizeFil].fil_sinopses);
-							system("@cls||clear");
-							printf("Digite a quantidade disponível:\n");
-							scanf("%d", &ListaFilmes[sizeFil].fil_qtd_disponivel);
-							ListaFilmes[sizeFil].fil_id = sizeFil;
-							sizeFil+=1;
-							system("@cls||clear");
-							printf("==================================================\n");
-							printf("Filme cadastrado com sucesso.\n");
-						break;
-						case 2:
-							system("@cls||clear");
-							printf("Digite o código do filme à editar:\n");
-							scanf("%d", &aux);
-							system("@cls||clear");
-							printf("Digite o novo nome do filme | Atual: %s:\n", ListaFilmes[aux].fil_nome);
-							scanf("%c", &val);
-							gets(ListaFilmes[aux].fil_nome);
-							system("@cls||clear");
-							printf("Digite a nova classificação | Atual: %d:\n", ListaFilmes[aux].fil_classificacao);
-							scanf("%d", &ListaFilmes[aux].fil_classificacao);
-							system("@cls||clear");
-							printf("Digite o novo gênero | Atual: %s:\n", (ListaFilmes[aux].fil_genero));
-							scanf("%c", &val);
-							gets(ListaFilmes[aux].fil_genero);
-							system("@cls||clear");
-							printf("Digite a nova sinopses | Atual: %s:\n", ListaFilmes[aux].fil_sinopses);
-							scanf("%c", &val);
-							gets(ListaFilmes[aux].fil_sinopses);
-							system("@cls||clear");
-							printf("Digite a nova quantidade disponível | Atual: %d:\n", ListaFilmes[aux].fil_qtd_disponivel);
-							scanf("%d", &ListaFilmes[aux].fil_qtd_disponivel);
-							system("@cls||clear");
-							printf("==================================================\n");
-							printf("Filme alterado com sucesso.\n");
-						break;
-						case 3:
-							system("@cls||clear");
+						
+
+for(i=1; i<100;i++){
+
+if (fl[i].fil_id== 0) {
+
+
+					printf("Digite o nome do filme: ");
+				   	fflush(stdin);
+				 	getchar();
+					fgets(fl[i].fil_nome, 40, stdin);
+					system("@cls||clear");
+					printf("Digite a sinopse do filme: ");
+					fflush(stdin);
+				 	getchar();
+					fgets(fl[i].fil_sinopses, 1000, stdin);
+				
+					fl[i].fil_id=i;
+					printf("Digite a quantidade disponivel em estoque: ");
+					scanf("%d",&fl[i].fil_qtd_disponivel);		
+					writeFilmes();
+					i=101;
+					printf("==================================================\n");
+					printf("Filme cadastrado com sucesso.\n");
+
+}else{
+
+}
+}
 
 						break;
+						case 2:
+						while(verif != 99){
+						verif = 0;
+						
+						printf("==================================================\n");
+						printf("Digite 1 para informar o ID do filme a ser editado\n");
+						printf("Digite 2 para pesquisar um filme para iditar\n");
+						printf("Digite 99 para voltar\n");
+						printf("==================================================\n");
+						scanf("%d", &verif);
+					switch(verif){
+							case 1:
+						
+						printf("Digite numero do ID do filme\n");
+						scanf("%d", &id);
+						editF(id);
+
+
+							break;
+							case 2:
+							break;
+					case 99:
+						system("@cls||clear");
+						printf("==================================================\n");
+						printf("Voltando...\n");
+							
+					break;
+				default:
+						system("@cls||clear");
+						printf("==================================================\n");
+						printf("Opção inválida, digite novamente!\n");
+							
+							break;
+					
+					
+					
+					}
+							
+							
+							
+							}
+						
+
+						break;
+						case 3:
+						system("@cls||clear");
+						printf("digite o para busca ");
+						scanf("%s", txc);
+						buscaF(txc);
+						break;
 						case 4:
-							system("@cls||clear");
-							printf("Digite o código do filme que deseja consultar:\n");
-							scanf("%d", &aux);
-							system("@cls||clear");
-							printf("ID: %d\n", ListaFilmes[aux].fil_id);
-							printf("Nome: %s\n", ListaFilmes[aux].fil_nome);
-							printf("Classificação: %d\n", ListaFilmes[aux].fil_classificacao);
-							printf("Gênero: %s\n", ListaFilmes[aux].fil_genero);
-							printf("Sinopse: %s\n", ListaFilmes[aux].fil_sinopses);
-							printf("Quantidade disponível: %d\n", ListaFilmes[aux].fil_qtd_disponivel);
+						system("@cls||clear");
+for(i=0; i<100;i++){
+
+if (fl[i].fil_id!=0){
+
+
+								printf("==================================================\n");
+							printf("Nome do FILME %s\n", fl[i].fil_nome);
+							printf("ID: %d\n", fl[i].fil_id);
+							printf("SINOPSE: %s", fl[i].fil_sinopses);
+							printf("Quantidade Disponivel em estoque: %d\n\n", fl[i].fil_qtd_disponivel);				
+								printf("==================================================\n");
+
+}}
+
 						break;
 						case 99:
 							system("@cls||clear");
@@ -198,38 +234,20 @@ void main(){
 			break;
 			case 3:
 				while(verif != 99){
-					printf("==================================================\n");
-					printf("Escolha uma opção:\n");
-					printf("1 - Locar filme;\n");
-					printf("2 - Gerar relatório;\n");
-					printf("99 - Voltar;\n");
-					printf("==================================================\n");
-					scanf("%d", &verif);
-					switch(verif){
-						case 1:
-							system("@cls||clear");
-							printf("Digite o código do cliente:\n");
-							scanf("%d", &aux);
-							ListaLocacao[sizeLoc].loc_cliId = ListaClientes[aux].cli_id;
-							system("@cls||clear");
-							printf("Digite o código do filme:\n");
-							scanf("%d", &aux);
-							ListaLocacao[sizeLoc].loc_filId = ListaFilmes[aux].fil_id;
-							ListaFilmes[aux].fil_qtd_disponivel-=1;
-							ListaLocacao[sizeLoc].loc_id = sizeLoc;
-							sizeLoc+=1;
 							system("@cls||clear");
 							printf("==================================================\n");
-							printf("Locação realizada com sucesso!\n");
+							printf("Escolha uma opção:\n");
+							printf("1 - Locar filme;\n");
+							printf("2 - Gerar relatório;\n");
+							printf("99 - Voltar;\n");
+							printf("==================================================\n");
+							scanf("%d", &verif);
+					switch(verif){
+						case 1:
+							
 						break;
 						case 2:
 							system("@cls||clear");
-							for(int i = 0; i<sizeLoc; i++){
-								printf("==================================================\n");
-								printf("Locação número: %d\n", i);
-								printf("Nome do cliente: %s\n", ListaClientes[ListaLocacao[i].loc_cliId].cli_nome);
-								printf("Nome do filme: %s\n", ListaFilmes[ListaLocacao[i].loc_filId].fil_nome);
-							}
 							printf("==================================================\n");
 							printf("Gerar PDF?\n");
 							printf("1 - Sim;\n");
@@ -237,7 +255,7 @@ void main(){
 							printf("==================================================\n");
 							scanf("%d", &verif);
 							if(verif == 1){
-								//Aqui gera o PDF
+								
 							}
 						break;
 						case 99:
@@ -257,15 +275,15 @@ void main(){
 				verif = 0;
 			break;
 			case 99:
-				system("@cls||clear");
-				printf("==================================================\n");
-				printf("Saindo...\n");
-				printf("==================================================\n");
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Saindo...\n");
+							printf("==================================================\n");
 			break;
 			default:
-				system("@cls||clear");
-				printf("==================================================\n");
-				printf("Opção inválida, digite novamente!\n");
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Opção inválida, digite novamente!\n");
 				
 			break;
 		}
