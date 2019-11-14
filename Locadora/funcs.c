c5580338ad2b6794d1aaa2b13bc6c140ac6c5521
ferreira-lucas <34555481+ferreira-lucas@users.noreply.github.com>
2019-11-14 13:03:14 -0300

    Add files via upload

--- Locadora/funcs.c
+++ Locadora/funcs.c
@@ -1,5 +1,5 @@
 #include <time.h>
-
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,124 +7,35 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include "funcs.h"
-    #include <string.h>
 
-
-void printF(int idf){
-
-printf("\n\nNome do FILME %s", fl[idf].fil_nome);
-printf("SINOPSE: %s", fl[idf].fil_sinopses);
-printf("ID: %d\n", fl[idf].fil_id);
-printf("Quantidade Disponivel em estoque: %d\n\n", fl[idf].fil_qtd_disponivel);
-
-
-}
-
-
-
-
-
-int buscaF(char busc[]){
-
-
-
-int i, item;
-int j=0;
-
-   char *psResultado;
-
- for(i=0; i<100; i++){
-   psResultado = strstr(fl[i].fil_nome, busc);
-
-        if(psResultado!= NULL){
-
-            printf("NOME: %s", fl[i].fil_nome);
-            printf("ID: %d\n\n", fl[i].fil_id);
-
-j++;
- }     else {}}
-   if(j==0){printf("\n\nNENHUM ITEM ENCONTRADO\n\n");}else{printf("\n\nFORAM ENCONTRADOS %d ITENS CORRESPONDENTES\n\n", j);
-   
-   
-   printf("\nDIGITE O ID CORRESPONDENTE A O ITEM PROCURADO\n");
-   scanf("%d", &item);
-   
-   
-   }
-   printF(item);
-                         return 0;
-}
-
-
-
-
-
-void loadClientes()
+void loadClientes(struct Cliente *c)
 {
 
     int red;
-    red = open("BancoC.txt", O_RDWR|O_CREAT, 0666);
+    red = open("Banco/Clientes.txt", O_RDWR|O_CREAT, 0666);
 
-   
+    printf("%d\n",red);
 
         if(red < 0){
             printf("ERROR\n");
         }
 
-    read(red, &cl, sizeof(struct cliente) * 1000);
+    read(red, c, sizeof(struct Cliente) * 1000);
     close(red);
 }
 
-
-void loadFilmes()
+void loadFilmes(struct Filme *f)
 {
 
     int red;
-    red = open("BancoF.txt", O_RDWR|O_CREAT, 0666);
+    red = open("Banco/Filmes.txt", O_RDWR|O_CREAT, 0666);
 
-   
+    printf("%d\n",red);
 
         if(red < 0){
             printf("ERROR\n");
         }
 
-    read(red, &fl, sizeof(struct filme) * 1000);
+    read(red, f, sizeof(struct Filme) * 1000);
     close(red);
 }
-
-void writeClientes(void)
-{
-
-    int red;
-    red = open("BancoC.txt", O_RDWR|O_CREAT, 0666);
-
-   
-
-        if(red < 0){
-            printf("ERROR\n");
-        }
-
-    write(red,&cl, sizeof(struct cliente) * 1000);
-    close(red);
-
-    printf("SUCESSO");
-}
-
-void writeFilmes(void)
-{
-
-    int red;
-    red = open("BancoF.txt", O_RDWR|O_CREAT, 0666);
-
-   
-
-        if(red < 0){
-            printf("ERROR\n");
-        }
-
-    write(red,&fl, sizeof(struct filme) * 1000);
-    close(red);
-
-    printf("SUCESSO");
-}
- 
--- Locadora/funcs.h
+++ Locadora/funcs.h
@@ -1,27 +1,17 @@
-   #include <time.h>
-    #include <string.h>
-    struct cliente{
-		int cli_id,cli_idade,cli_cpf;
-		char cli_nome[100],cli_endereco[500];
-	};
+struct Cliente{
+	int cli_id, cli_idade;
+	long cli_cpf;
+	char cli_nome[100], cli_endereco[500];
+};
 
-	struct filme{
-		int fil_id, fil_classificacao, fil_qtd_disponivel;
-		char fil_nome[500], fil_genero[100], fil_sinopses[3000];
-	};
+struct Filme{
+	int fil_id, fil_classificacao, fil_qtd_disponivel;
+	char fil_nome[500], fil_genero[100], fil_sinopses[3000];
+};
 
-	struct locacao{
-		int loc_id, loc_cliId, loc_filId;
-		struct tm loc_data_locacao;
-	};
-struct filme fl[100];
-struct cliente cl[100];
-	void writeFilmes(void);
-
-	void writeClientes();
-
-	void loadFilmes(void);
-
-	void loadClientes();
-	
-	int buscaF(char busc[]);
+struct Locacao{
+	int loc_id, loc_cliId, loc_filId;
+	struct tm loc_data_locacao;
+};
+void loadClientes(struct Cliente *c);
+void loadFilmes(struct Filme *f);
