104ae22aef164d83a3dd6b1b4355644fa5beea99
VlNlClUS <56849654+VlNlClUS@users.noreply.github.com>
2019-11-14 15:46:19 -0300

    inserdo busca filmes e edit filmes
    atualizado

---
+++ funcs.h
@@ -0,0 +1,28 @@
+   #include <time.h>
+    #include <string.h>
+    struct cliente{
+		int cli_id,cli_idade,cli_cpf;
+		char cli_nome[100],cli_endereco[500];
+	};
+
+	struct filme{
+		int fil_id, fil_classificacao, fil_qtd_disponivel;
+		char fil_nome[500], fil_genero[100], fil_sinopses[3000];
+	};
+
+	struct locacao{
+		int loc_id, loc_cliId, loc_filId;
+		struct tm loc_data_locacao;
+	};
+struct filme fl[100];
+struct cliente cl[100];
+	void writeFilmes(void);
+
+	void writeClientes();
+
+	void loadFilmes(void);
+
+	void loadClientes();
+	
+	int buscaF(char busc[]);
+	void editF(int idf);
---
+++ main.c
@@ -0,0 +1,291 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "funcs.h"
+#include <string.h>
+#include <locale.h>
+void main(){
+	int verif = 0;
+	char val;
+	int i,j,id;
+	char txc[10];
+
+loadFilmes();
+	system("@cls||clear");
+	
+
+	printf("==================================================\n");
+	printf("BEM VINDOS A LOCADORA 9/10 PORQUE NEM TUDO É PERFEITO\n");
+	printf("   A \n");
+	printf("  *** \n");
+	printf(" *o*o* \n");
+	printf("*o***o* \n");
+	printf("   |\n");
+	printf("\n");
+	printf("Feliz Natal e um ótimo ano novo!! Aceitamos nota de presente ou um vale refeição.\n");
+		
+	while(verif != 99){
+		printf("==================================================\n");
+		printf("Escolha uma opção:\n");
+		printf("1 - Clientes;\n");
+		printf("2 - Filmes;\n");
+		printf("3 - Locação;\n");
+		printf("99 - Sair;\n");
+		printf("==================================================\n");
+		scanf("%d", &verif);
+		system("@cls||clear");
+		switch(verif){
+			case 1:
+				while(verif != 99){
+					printf("==================================================\n");
+					printf("Escolha uma opção:\n");
+					printf("1 - Cadastrar cliente;\n");
+					printf("2 - Editar cliente;\n");
+					printf("99 - Voltar;\n");
+					printf("==================================================\n");
+					scanf("%d", &verif);
+					switch(verif){
+						case 1:
+					
+for(i=1; i<100;i++){
+
+if (fl[i].fil_id== 0) {
+
+
+					printf("Digite o nome do cliente: ");
+				   		fflush(stdin);
+				 		getchar();
+						fgets(cl[i].cli_nome, 40, stdin);
+						system("@cls||clear");
+					printf("Digite o endereço do cliente: ");
+				   		fflush(stdin);
+				 		getchar();
+						fgets(cl[i].cli_endereco, 40, stdin);
+					printf("Digite o ID do cliente: ");
+						scanf("%d",&cl[i].cli_id);
+					printf("Digite CPF do cliente (SOMENTE NUMEROS): ");
+						scanf("%d",&cl[i].cli_cpf);
+					printf("Digite a idade do cliente: ");
+						scanf("%d",&cl[i].cli_idade);		
+						writeClientes();
+i=101;
+					
+					}
+}
+						
+						printf("==================================================\n");
+						printf("Cliente cadastrado com sucesso.\n");
+						break;
+						case 2:
+
+										
+
+
+						break;
+						
+
+	
+						
+						case 99:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Voltando...\n");
+							
+						break;
+						default:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Opção inválida, digite novamente!\n");
+							
+						break;
+					}
+				}
+				verif = 0;
+			break;
+			case 2:
+				while(verif != 99){
+					printf("==================================================\n");
+					printf("Escolha uma opção:\n");
+					printf("1 - Cadastrar filme;\n");
+					printf("2 - Editar filme;\n");
+					printf("3 - Procurar filme;\n");
+					printf("4 - Consultar informações;\n");
+					
+						
+					printf("99 - Voltar;\n");
+					printf("==================================================\n");
+					scanf("%d", &verif);
+					switch(verif){
+						case 1:
+						
+
+for(i=1; i<100;i++){
+
+if (fl[i].fil_id== 0) {
+
+
+					printf("Digite o nome do filme: ");
+				   	fflush(stdin);
+				 	getchar();
+					fgets(fl[i].fil_nome, 40, stdin);
+					system("@cls||clear");
+					printf("Digite a sinopse do filme: ");
+					fflush(stdin);
+				 	getchar();
+					fgets(fl[i].fil_sinopses, 1000, stdin);
+				
+					fl[i].fil_id=i;
+					printf("Digite a quantidade disponivel em estoque: ");
+					scanf("%d",&fl[i].fil_qtd_disponivel);		
+					writeFilmes();
+					i=101;
+					printf("==================================================\n");
+					printf("Filme cadastrado com sucesso.\n");
+
+}else{
+
+}
+}
+
+						break;
+						case 2:
+						while(verif != 99){
+						verif = 0;
+						
+						printf("==================================================\n");
+						printf("Digite 1 para informar o ID do filme a ser editado\n");
+						printf("Digite 2 para pesquisar um filme para iditar\n");
+						printf("Digite 99 para voltar\n");
+						printf("==================================================\n");
+						scanf("%d", &verif);
+					switch(verif){
+							case 1:
+						
+						printf("Digite numero do ID do filme\n");
+						scanf("%d", &id);
+						editF(id);
+
+
+							break;
+							case 2:
+							break;
+					case 99:
+						system("@cls||clear");
+						printf("==================================================\n");
+						printf("Voltando...\n");
+							
+					break;
+				default:
+						system("@cls||clear");
+						printf("==================================================\n");
+						printf("Opção inválida, digite novamente!\n");
+							
+							break;
+					
+					
+					
+					}
+							
+							
+							
+							}
+						
+
+						break;
+						case 3:
+						system("@cls||clear");
+						printf("digite o para busca ");
+						scanf("%s", txc);
+						buscaF(txc);
+						break;
+						case 4:
+						system("@cls||clear");
+for(i=0; i<100;i++){
+
+if (fl[i].fil_id!=0){
+
+
+								printf("==================================================\n");
+							printf("Nome do FILME %s\n", fl[i].fil_nome);
+							printf("ID: %d\n", fl[i].fil_id);
+							printf("SINOPSE: %s", fl[i].fil_sinopses);
+							printf("Quantidade Disponivel em estoque: %d\n\n", fl[i].fil_qtd_disponivel);				
+								printf("==================================================\n");
+
+}}
+
+						break;
+						case 99:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Voltando...\n");	
+							
+						break;
+						default:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Opção inválida, digite novamente!\n");
+							
+						break;
+					}
+				}
+				verif = 0;
+			break;
+			case 3:
+				while(verif != 99){
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Escolha uma opção:\n");
+							printf("1 - Locar filme;\n");
+							printf("2 - Gerar relatório;\n");
+							printf("99 - Voltar;\n");
+							printf("==================================================\n");
+							scanf("%d", &verif);
+					switch(verif){
+						case 1:
+							
+						break;
+						case 2:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Gerar PDF?\n");
+							printf("1 - Sim;\n");
+							printf("2 - Não;\n");
+							printf("==================================================\n");
+							scanf("%d", &verif);
+							if(verif == 1){
+								
+							}
+						break;
+						case 99:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Voltando...\n");
+							
+						break;
+						default:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Opção inválida, digite novamente!\n");
+							
+						break;
+					}
+				}
+				verif = 0;
+			break;
+			case 99:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Saindo...\n");
+							printf("==================================================\n");
+			break;
+			default:
+							system("@cls||clear");
+							printf("==================================================\n");
+							printf("Opção inválida, digite novamente!\n");
+				
+			break;
+		}
+	}
+}
